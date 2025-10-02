fa50fe6182b233125a4c06886b99e407ae6467f9
uselesscato <phatta.22ce@vku.udn.vn>
2025-10-02 18:22:08 +0700

    firstpush

---
+++ config/.config/fish/config.fish
@@ -0,0 +1,7 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+oh-my-posh init fish --config /home/nekomeow/.config/fish/catppuccin_mocha.omp.json | source
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
